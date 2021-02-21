b15da39f4b91591204b6571c1ea0ea0f2dfd1ae8
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-22 00:13:27 +0100

    Developed the class StudentBl. close # 3

--- src/com/netmind/business/StudentBl.java
+++ src/com/netmind/business/StudentBl.java
@@ -1,5 +1,30 @@
 package com.netmind.business;
 
+import java.util.Calendar;
+
 public class StudentBl {
-    
+
+    public static int calculateAge(String dateOfBirth) {
+
+        // Calculo de la edad
+        Calendar cal = Calendar.getInstance();
+
+        String[] dateOfBirthSeparated = dateOfBirth.split("/");
+
+        Integer yearOfbirth = Integer.parseInt(dateOfBirthSeparated[2]);
+        Integer actualYear = cal.get(Calendar.YEAR);
+
+        Integer monthOfBirth = Integer.parseInt(dateOfBirthSeparated[1]);
+        Integer actualMonth = cal.get(Calendar.MONTH);
+
+        Integer ageCalculated = actualYear - yearOfbirth;
+
+        if (actualMonth < monthOfBirth) {
+            ageCalculated -= 1;
+        }
+
+        return ageCalculated;
+
+    }
+
 }
