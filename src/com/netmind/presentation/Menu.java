149aec7e5c00216df0281c20380604751918711f
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-22 00:21:09 +0100

    Developed class Menu

--- src/com/netmind/presentation/Menu.java
+++ src/com/netmind/presentation/Menu.java
@@ -1,5 +1,52 @@
 package com.netmind.presentation;
 
-public class Menu{
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.UUID;
+
+import com.netmind.model.Student;
+import com.netmind.business.StudentBl;
+import com.netmind.dao.StudentDao;
+
+public class Menu {
+
+    public static int hello() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "******** MENU ********\n\nWellcome, what you want to do?\n\n1. Add new student.\n2. Calculate oldest student.\n3. Calculate the average age of students.\n4. Exit");
+        int decission = scanner.nextInt();
+        return decission;
+    }
+
+    public static void askNewStudent(){
+        Scanner scanner = new Scanner(System.in);
+
+        String id = UUID.randomUUID().toString();
+        
+        System.out.println("Enter the name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter the surname:");
+        String surname = scanner.nextLine();
+
+        System.out.println("Enter the date of birth (dd/mm/yyyy):");
+        String dateOfBirth = scanner.nextLine();
+
+        Integer age = StudentBl.calculateAge(dateOfBirth);
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date dateOfBirthToDate;
+        try {
+            dateOfBirthToDate = format.parse(dateOfBirth);
+            Student newStudent = new Student(id, name, surname, age, dateOfBirthToDate );
+            StudentDao.add(newStudent);
+            StudentDao.read();
     
+
+        } catch (ParseException ex) {
+            System.out.println(ex);
+        }
+    }
 }
