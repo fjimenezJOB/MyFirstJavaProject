cbf77c6a5953213cea61d614b84a307c5a21511b
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-22 00:20:39 +0100

    Created the methods add and read. close #4

--- src/com/netmind/dao/StudentDao.java
+++ src/com/netmind/dao/StudentDao.java
@@ -1,5 +1,21 @@
 package com.netmind.dao;
 
+import java.util.ArrayList;
+
+import com.netmind.model.Student;
+
 public class StudentDao {
     
+    public static ArrayList<Student> students = new ArrayList<Student>();
+
+    public static void add(Student student) {
+        students.add(student);
+    }
+
+    public static void read() {
+        for (int iterator = 0; iterator < students.size(); iterator++) {
+            System.out.println(students.get(iterator));
+        }
+    }
+
 }
