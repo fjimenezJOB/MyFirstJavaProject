7a543f03562a66089ac6ff871f54059cfad738ab
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-22 14:23:07 +0100

    The application does not close until the user chooses option 4 in the menu. close #8

--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -3,14 +3,24 @@ package com.netmind.presentation;
 public class Principal {
     public static void main(String[] args) {
 
-        int decission = Menu.hello();
-        switch (decission) {
-            case 1:
-                Menu.askNewStudent();
-                break;
-            case 2:
-                break;
-        }
+        Boolean interruptor = true;
 
+        while (interruptor) {
+
+            int decission = Menu.hello();
+
+            switch (decission) {
+                case 1:
+                    Menu.askNewStudent();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    interruptor = false;
+
+            }
+        }
     }
 }
