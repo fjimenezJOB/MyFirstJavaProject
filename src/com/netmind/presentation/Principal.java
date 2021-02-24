c789b3421a24574d2b4c1098839234faa3e6b165
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-24 01:19:15 +0100

    Refactor in class Principal. close #12

--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -1,29 +1,8 @@
 package com.netmind.presentation;
 
 public class Principal {
-    public static void main(String[] args) {
 
-        Boolean interruptor = true;
-
-        do {
-
-            int decission = Menu.hello();
-
-            switch (decission) {
-                case 1:
-                    Menu.askNewStudent();
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    interruptor = false;
-                    break;
-                default:
-                    Menu.hello();
-                    break;
-            }
-        } while (interruptor);
-    }
+  public static void main(String[] args) {
+    Menu.decission();
+  }
 }
