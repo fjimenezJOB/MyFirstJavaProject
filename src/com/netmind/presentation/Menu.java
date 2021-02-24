56447932197637cd6f1fd88d503b5bc87c2cf330
Fran Jimenez <fjimenezjob@gmail.com>
2021-02-24 03:23:16 +0100

    Refactor in class Menu added method decision. close #13

--- src/com/netmind/presentation/Menu.java
+++ src/com/netmind/presentation/Menu.java
@@ -1,52 +1,78 @@
 package com.netmind.presentation;
 
+import com.netmind.business.StudentBl;
+import com.netmind.model.Student;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 import java.util.UUID;
 
-import com.netmind.model.Student;
-import com.netmind.business.StudentBl;
-import com.netmind.dao.StudentDao;
-
 public class Menu {
 
-    public static int hello() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(
-                "******** MENU ********\n\nWellcome, what you want to do?\n\n1. Add new student.\n2. Calculate oldest student.\n3. Calculate the average age of students.\n4. Exit");
-        int decission = scanner.nextInt();
-        return decission;
-    }
+  public static int hello() {
 
-    public static void askNewStudent(){
-        Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(
+        "******** MENU ********\n\nWellcome, what you want to do?\n\n1. Add new student.\n2. Calculate oldest student.\n3. Calculate the average age of students.\n4. Exit");
+    int decission = scanner.nextInt();
 
-        String id = UUID.randomUUID().toString();
-        
-        System.out.println("Enter the name:");
-        String name = scanner.nextLine();
+    return decission;
+  }
 
-        System.out.println("Enter the surname:");
-        String surname = scanner.nextLine();
+  public static void decission() {
 
-        System.out.println("Enter the date of birth (dd/mm/yyyy):");
-        String dateOfBirth = scanner.nextLine();
+    Boolean interruptor = true;
 
-        Integer age = StudentBl.calculateAge(dateOfBirth);
+    try {
 
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        Date dateOfBirthToDate;
-        try {
-            dateOfBirthToDate = format.parse(dateOfBirth);
-            Student newStudent = new Student(id, name, surname, age, dateOfBirthToDate );
-            StudentDao.add(newStudent);
-            StudentDao.read();
-    
+      do {
+        int decission = Menu.hello();
 
-        } catch (ParseException ex) {
-            System.out.println(ex);
+        switch (decission) {
+          case 1:
+            Student student = new Student();
+            Menu.askNewStudent(student);
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          case 4:
+            interruptor = false;
+
+            break;
+          default:
+            Menu.hello();
         }
+      } while (interruptor);
+    } catch (Exception ex) {
+
+      System.out.println(ex + " - " + "Try to insert a number of options in menu.");
     }
+  }
+
+  public static void askNewStudent(Student student) {
+    StudentBl studentBl = new StudentBl();
+
+    Scanner scanner = new Scanner(System.in);
+    student.setIdStudent(UUID.randomUUID().toString());
+
+    System.out.println("Enter the name:");
+    student.setName(scanner.nextLine());
+
+    System.out.println("Enter the surname:");
+    student.setSurname(scanner.nextLine());
+
+    System.out.println("Enter the date of birth (dd/mm/yyyy):");
+
+    try {
+
+      student.setDateOfBirth(new SimpleDateFormat("dd/MM/yyyy").parse(scanner.nextLine()));
+      studentBl.add(student);
+
+    } catch (ParseException ex) {
+      System.out.println(ex.getMessage());
+      ex.printStackTrace();
+    }
+  }
 }
