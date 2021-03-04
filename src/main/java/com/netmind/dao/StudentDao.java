e4c8529c307b59fe13a21ad0cb3338fe909c7ea4
Fran Jimenez <fjimenezjob@gmail.com>
2021-03-04 22:49:25 +0100

    Implemented the config.properties in the project close #33

--- src/main/java/com/netmind/dao/StudentDao.java
+++ src/main/java/com/netmind/dao/StudentDao.java
@@ -8,25 +8,44 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.UUID;
 
 public class StudentDao {
 
   final static Logger logger = Logger.getLogger(StudentDao.class);
+  static Properties prop = null;
+  static InputStream input = null;
+
+  static{
+    prop = new Properties();
+    try {
+      input = StudentDao.class.getResourceAsStream("/config.properties");
+      prop.load(input);
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      throw new ExceptionInInitializerError(e);
+    }
+  }
+
 
   public boolean add(Student student) {
     return addStudentToFile(student);
   }
 
   public boolean addStudentToFile(Student student) {
-    try (FileWriter writer = new FileWriter("output\\students.txt", true);) {
+    // Llamando al archivo properties para cargar el nombre del archivo
+    try (FileWriter writer = new FileWriter(prop.getProperty("TxtFilename"), true);) {
+      logger.info("Creating file if not exists");
       logger.info("Starting add Student in the txt file");
       writer.write(student.toTxtFile());
       writer.write(System.lineSeparator());
+      logger.info("The sutudent is created");
       return true;
     } catch (IOException e) {
       e.printStackTrace();
@@ -36,7 +55,7 @@ public class StudentDao {
   }
 
   public ArrayList<Student> readAllStudentTxt() throws IOException, ParseException {
-    try (FileReader reader = new FileReader("output\\students.txt")) {
+    try (FileReader reader = new FileReader(prop.getProperty("TxtFilename"))) {
       if (reader.ready()) {
         logger.info("Starting read the txt file");
         try {
